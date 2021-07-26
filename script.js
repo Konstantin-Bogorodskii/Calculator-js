adeb1f844dcf423396fd92a68ce392fdc87e9092
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-07-27 01:16:33 +0300

    rewrote the code

--- script.js
+++ script.js
@@ -1,72 +1,100 @@
 'use strict';
-const hourEl = document.querySelector('.clock__hour');
-const minuteEl = document.querySelector('.clock__minute');
-const valueEl = document.querySelector('.value');
+
 //functions of calculator
-const acEl = document.querySelector('#ac');
-const pmEl = document.querySelector('#pm');
-const percentEl = document.querySelector('#percent');
+// const acEl = document.querySelector('#ac');
+// const pmEl = document.querySelector('#pm');
+// const percentEl = document.querySelector('#percent');
 
-//operators of calculator
-const divisionEl = document.querySelector('#division');
-const multiEl = document.querySelector('#multi');
-const minusEl = document.querySelector('#minus');
-const plusEl = document.querySelector('#plus');
-const equalEl = document.querySelector('#equal');
+// //operators of calculator
+// const divisionEl = document.querySelector('#division');
+// const multiEl = document.querySelector('#multi');
+// const minusEl = document.querySelector('#minus');
+// const plusEl = document.querySelector('#plus');
+// const equalEl = document.querySelector('#equal');
 
-//numbers of calculator
-const sevenEl = document.querySelector('#seven');
-const eightEl = document.querySelector('#eight');
-const nineEl = document.querySelector('#nine');
-const fourEl = document.querySelector('#four');
-const fiveEl = document.querySelector('#five');
-const sixEl = document.querySelector('#six');
-const oneEl = document.querySelector('#one');
-const twoEl = document.querySelector('#two');
-const threeEl = document.querySelector('#three');
-const zeroEl = document.querySelector('#zero');
-const dotEl = document.querySelector('#dot');
+// //numbers of calculator
+// const sevenEl = document.querySelector('#seven');
+// const eightEl = document.querySelector('#eight');
+// const nineEl = document.querySelector('#nine');
+// const fourEl = document.querySelector('#four');
+// const fiveEl = document.querySelector('#five');
+// const sixEl = document.querySelector('#six');
+// const oneEl = document.querySelector('#one');
+// const twoEl = document.querySelector('#two');
+// const threeEl = document.querySelector('#three');
+// const zeroEl = document.querySelector('#zero');
+// const dotEl = document.querySelector('#dot');
 
-// Addeventlistener on Numbers
-const numberElArray = [
-  zeroEl,
-  oneEl,
-  twoEl,
-  threeEl,
-  fourEl,
-  fiveEl,
-  sixEl,
-  sevenEl,
-  eightEl,
-  nineEl,
-];
+// // Addeventlistener on Numbers
+// const numberElArray = [
+//   zeroEl,
+//   oneEl,
+//   twoEl,
+//   threeEl,
+//   fourEl,
+//   fiveEl,
+//   sixEl,
+//   sevenEl,
+//   eightEl,
+//   nineEl,
+// ];
 
-// const getValueAsStr = () => {
-//   const currentDisplayStr = valueEl.textContent;
-//   console.log(currentDisplayStr);
-//   if (currentDisplayStr !== '0') {
-//     return currentDisplayStr.split(' ').join(' ');
-//   }
-// };
+const calculator = document.querySelector('.calculator');
+const display = calculator.querySelector('.display');
+const buttonsContainer = calculator.querySelector('.buttons-container');
 
-const handleNumberClick = numStr => {
-  const currentDisplayStr = getValueAsStr();
+// Add Deligation event on buttons wrap.
+buttonsContainer.addEventListener('click', event => {
+  let key = event.target;
+  const { type } = key.dataset;
+  // Get all data-type = ".."
+  const { previousKeyType } = calculator.dataset;
 
-  if (currentDisplayStr == '0') {
-    valueEl.textContent = numStr;
-  } else {
-    valueEl.textContent = parseFloat(currentDisplayStr + numStr).toLocaleString();
+  if (!key.closest('button')) return;
+  // Exclude grid-gap of target-list
+
+  let keyValue = key.textContent;
+  const displayValue = display.textContent;
+
+  // If this a Number key?
+  if (type == 'number') {
+    if (displayValue == '0') {
+      display.textContent = keyValue;
+    } else if (previousKeyType == 'operator') {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
   }
-};
 
-for (let i = 0; i < numberElArray.length; i++) {
-  const numberEl = numberElArray[i];
-  numberEl.addEventListener('click', () => {
-    handleNumberClick(i.toString());
-  });
-}
+  // If this an Operator key?
+  if (type == 'operator') {
+    const operatorKeys = document.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach(item => {
+      item.dataset.state = '';
+    });
+    key.dataset.state = 'selected';
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  // If this an Equal key?
+  // We need to numbers First and second to get some math operations FirstNumber * SecondNumber and etc.
+  if (type == 'equal') {
+    const secondNumber = displayValue;
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    console.log(firstNumber, secondNumber, operator);
+  }
+
+  calculator.dataset.previousKeyType = type;
+  // Refresh dataset.previousKeyType after every operation
+});
 
 // Set up the time
+
+const hourEl = document.querySelector('.clock__hour');
+const minuteEl = document.querySelector('.clock__minute');
 
 const updateTime = () => {
   const currentTime = new Date();
@@ -80,4 +108,4 @@ const updateTime = () => {
 setInterval(updateTime, 1000);
 updateTime();
 // Function is called once after delay per second, so we need new func to update time every time (updateTime)
-// We set all code from setInterval to updateTime and call updateTime in set inteveal every second;
+// We set all code from setInterval to updateTime and call updateTime in setInteveal every second;
